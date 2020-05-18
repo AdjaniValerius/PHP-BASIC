14a4164aa2254b33ba298d3757e2dacfe18f101f
Adjani Valerius <2068687@talnet.nl>
2020-05-18 14:12:19 +0100

    taak 6

---
+++ 2-Variabelen-en-datatype/taak06/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$voornaam = "Adjani";
+    echo gettype($voornaam);
+$variable2 = 1;
+    echo gettype($variable2);
+$variable3 = 1.0;
+    echo gettype($variable3);
+$variable4 = true;
+    echo gettype($variable4);
+$variable5 = null;
+    echo gettype($variable5);
+
+?>
