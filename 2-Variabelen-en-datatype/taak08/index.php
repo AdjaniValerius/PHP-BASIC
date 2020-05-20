dd3fa3dcdb3b4da08777dec2c4d1932651561baf
Adjani Valerius <2068687@talnet.nl>
2020-05-20 11:08:44 +0100

    taak 8 af

--- 2-Variabelen-en-datatype/taak08/index.php
+++ 2-Variabelen-en-datatype/taak08/index.php
@@ -4,8 +4,6 @@ $voornaam1 = "Adjani";
 $voornaam2 = "Tim";
 $voornaam3 = "Joris";
 
-<ul>
-    echo "<li>$voornaam1</li><li>$voornaam2</li><li>$voornaam3</li>";
-</ul>
+    echo "<ul><li>$voornaam1</li><li>$voornaam2</li><li>$voornaam3</li></ul>";
 
 ?>
