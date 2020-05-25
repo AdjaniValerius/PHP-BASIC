39e31df47a75e66fabf979f50c1e6a87fe1c33f8
Adjani Valerius <2068687@talnet.nl>
2020-05-25 12:52:52 +0100

    4 - taak 1

---
+++ 4-Arithmetics/taak01/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$getal1 = 2;
+$getal2 = 3;
+    echo $getal1 + $getal2;
+
+$getal1 = 3;
+$getal2 = 2;
+    echo $getal1 - $getal2;
+
+$getal1 = 2;
+$getal2 = 3;
+    echo $getal1 * $getal2;
+
+$getal1 = 50;
+$getal2 = 5;
+    echo $getal1 / $getal2;
+
+?>
