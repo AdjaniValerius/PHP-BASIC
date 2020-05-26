73c4d73a2166a243f7217189327216e4c3bbb23b
Adjani Valerius <2068687@talnet.nl>
2020-05-26 11:10:43 +0100

    5 - taak 2

--- 5-Conditional_Statements/taak02/index.php
+++ 5-Conditional_Statements/taak02/index.php
@@ -1,5 +1,61 @@
 <?php
 
+$test1 = 10;
+$test2 = 6;
+
+if($test1 == $test2){
+    echo "gelijk";
+}
+
+
+$test1 = 10;
+$test2 = 6;
+
+if($test1 = $test2){
+    echo "ongelijk";
+}
+
+
+$test1 = 10;
+$test2 = 6;
+
+if($test1 != $test2){
+    echo "identiek";
+}
+
+
+$test1 = 10;
+$test2 = 6;
+
+if($test1 > $test2){
+    echo "test1 is groter dan test2";
+}
+
+
+$test1 = 10;
+$test2 = 6;
+
+if($test1 < $test2){
+    echo "test1 is kleiner dan test2";
+}
+
+
+$test1 = 10;
+$test2 = 6;
+
+if($test1 >= $test2){
+    echo "test1 is groter of gelijk aan test2";
+}
+
+
+$test1 = 10;
+$test2 = 6;
+
+if($test1 <= $test2){
+    echo "test1 is kleiner of gelijk aan test2";
+}
+
+
 
 
 ?>
