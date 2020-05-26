566c8945a0b3777ed3f1fc7bf43d540f24a3109c
Adjani Valerius <2068687@talnet.nl>
2020-05-26 11:46:25 +0100

    5 - taak3

--- 5-Conditional_Statements/taak03/index.php
+++ 5-Conditional_Statements/taak03/index.php
@@ -1,5 +1,79 @@
 <?php
 
+$test1 = 3;
+$test2 = 6;
 
+if($test1 == $test2){
+    echo "<ul><li>gelijk</li></ul>" ;
+}
+else{
+    echo "<ul><li>niet gelijk</li></ul>";
+}
+
+
+$test1 = 3;
+$test2 = 6;
+
+if($test1 = $test2){
+    echo "<ul><li>ongelijk<li></ul>";
+}
+else{
+    echo "<ul></li>niet ongelijk<li></ul>";
+}
+
+
+$test1 = 3;
+$test2 = 6;
+
+if($test1 != $test2){
+    echo "<ul><li>identiek<li></ul>";
+}
+else{
+    echo "<ul></li>apart<li></ul>";
+}
+
+
+$test1 = 3;
+$test2 = 6;
+
+if($test1 > $test2){
+    echo "<ul><li>groter dan<li></ul>";
+}
+else{
+    echo "<ul></li>niet groter dan<li></ul>";
+}
+
+
+$test1 = 3;
+$test2 = 6;
+
+if($test1 < $test2){
+    echo "<ul><li>kleiner dan<li></ul>";
+}
+else{
+    echo "<ul></li>niet kleiner dan<li></ul>";
+}
+
+
+$test1 = 3;
+$test2 = 6;
+
+if($test1 >= $test2){
+    echo "<ul><li>groter en gelijk dan<li></ul>";
+}
+else{
+    echo "<ul></li>niet groter en gelijk dan<li></ul>";
+}
+
+
+$test1 = 3;
+$test2 = 6;
+
+if($test1 <= $test2){
+    echo "<ul><li>kleiner en gelijk dan<li></ul>";
+}
+else{
+    echo "<ul></li>niet kleier en gelijk dan<li></ul>";
+}
 
 ?>
