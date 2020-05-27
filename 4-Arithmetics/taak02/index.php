7f85326e3e9f4b5f1571c158c9490ac934ba005f
Adjani Valerius <2068687@talnet.nl>
2020-05-27 12:27:45 +0100

    4 - taak 2

--- 4-Arithmetics/taak02/index.php
+++ 4-Arithmetics/taak02/index.php
@@ -1,13 +1,15 @@
 <?php
 
+$getal1 = 0;
+
 $getal1 += 5;
-    echo $getal1;
+    echo $getal1 . "<br>";
 
 $getal1 -= 5;
-    echo $getal1;
+    echo $getal1 . "<br>";
 
 $getal1 *= 5;
-    echo $getal1;
+    echo $getal1 . "<br>";
 
 $getal1 /= 5;
     echo $getal1;
--- 5-Conditional_Statements/taak04/index.php
+++ 5-Conditional_Statements/taak04/index.php
@@ -1,5 +1,22 @@
 <?php
 
+$prod_besteld = 8;
 
+    if($prod_besteld < 10) {
+        $prijs = 1.50;
+}
+
+//$prod_besteld = 8;
+
+    elseif($prod_besteld < 20) {
+        $prijs = 1.25;
+}
+
+//$prod_besteld = 8;
+    else {
+        $prijs = 1.00;
+}
+
+echo $prijs;
 
 ?>
