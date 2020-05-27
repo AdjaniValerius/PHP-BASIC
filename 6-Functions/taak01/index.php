a9f95bdff2634de8bc95d17cca030f0db990a7ef
Adjani Valerius <2068687@talnet.nl>
2020-05-27 15:22:00 +0100

    6 - taak 1

---
+++ 6-Functions/taak01/index.php
@@ -0,0 +1,12 @@
+<?php
+
+function zetTweeWoordenAanElkaar(){
+    $woordeje = "Hallo";
+    $zinnetje = "mooie blauwe planeet!";
+    $heleZin = "Hallo mooie blauwe planeet!";
+        echo $heleZin;
+}
+
+zetTweeWoordenAanElkaar()
+
+?>
