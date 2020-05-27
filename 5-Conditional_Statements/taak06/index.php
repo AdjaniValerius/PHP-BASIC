0e5674d2568b539d8c04af26f088a27aca2ccf94
Adjani Valerius <2068687@talnet.nl>
2020-05-27 14:04:33 +0100

    5 - taak 6

--- 5-Conditional_Statements/taak06/index.php
+++ 5-Conditional_Statements/taak06/index.php
@@ -1,5 +1,41 @@
 <?php
 
+$var1 = 8;
 
+if($var1 < 10 && $var1 > 5) {
+    echo "het getal zit tussen de 10 en de 5" . "<br>";
+} else {
+    echo "het getal is groter dan 10 of kleiner dan 5!" . "<br>";
+}
+
+//--------------
+
+$var1 = 8;
+
+if($var1 < 10 || $var1 > 5) {
+    echo "het getal zit tussen de 10 en de 5". "<br>";
+} else {
+    echo "het getal is groter dan 10 of kleiner dan 5!" . "<br>";
+}
+
+//--------------
+
+$var1 = 8;
+
+if($var1 < 10 xor $var1 > 5) {
+    echo "het getal zit tussen de 10 en de 5". "<br>";
+} else {
+    echo "het getal is groter dan 10 of kleiner dan 5!" . "<br>";
+}
+
+//--------------
+
+$var1 = 8;
+
+if($var1 < 10 ! $var1 > 5) {
+    echo "het getal zit niet tussen de 10 en de 5". "<br>";
+} else {
+    echo "het getal is groter dan 10 of kleiner dan 5!" . "<br>";
+}
 
 ?>
