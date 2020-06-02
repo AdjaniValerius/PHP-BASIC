58b7ea6392e0c0204f570265df8624f853f290b4
Adjani Valerius <2068687@talnet.nl>
2020-06-02 14:36:27 +0100

    eindopdracht in progress

--- opdracht/index.php
+++ opdracht/index.php
@@ -1,13 +1,18 @@
 <!doctype html>
 <html lang="en">
+
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Title</title>
   </head>
-    <form action="index.php" method="get"><!-- dit laten staan! -->
+  <body>
 
+  <?php include 'menu.php';  
+  ?>
+
+    <form action="index.php" method="get"><!-- dit laten staan! -->
     <p>Op deze website kun je checken welk datatype je hebt ingevoerd.</p>
     <P>Vul hieronder een getal, een decimaal, een tekst of de tekst true/false in en klik op check!</p> 
     <input type="text" name="input"><!-- dit laten staan! -->
@@ -15,5 +20,11 @@
     <p>Het datatype van de waarde: <?php getInput(); ?> is </p>
 
     </form><!-- dit laten staan! -->
+
+    <footer>
+    <?php include 'footer.php';
+    ?>
+    </footer>
+
   </body>
 </html>
--- opdracht/producten.php
+++ opdracht/producten.php
@@ -6,9 +6,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Title</title>
   </head>
+  <body>
+
+  <?php 
+  include 'menu.php'; 
+  require 'includes/functions.php'; 
+  ?>
+
+
+
     <form action="producten.php" method="get"><!-- dit laten staan! -->
 
     <p>Hieronder een lijst met producten die je bij ons kunt bestellen en de prijs per stuk:</p>
+        
+<?php $itemsProducten = array ("appels", "peren", "ananas", "banaan", "sinasappel", "perzik", "kersen", "aardbeien", "mango", "druif");
+      $itemsPrijs = array ("0,10", "0.15", "0.20", "0.25", "0.30", "0.35", "0.40", "0.45", "0.50", "0.60");
+?>
+
         <table>
             <tr>
                 <th>#</th>
@@ -18,12 +32,52 @@
             <tr>
                 <td>0</td>
                 <td>appels</td>
-                <td>0,25</td>
+                <td>0,10</td>
             </tr>
             <tr>
                 <td>1</td>
                 <td>peren</td>
+                <td>0,15</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>ananas</td>
+                <td>0,20</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>banaan</td>
+                <td>0,25</td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>sinasappel</td>
                 <td>0,30</td>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td>perzik</td>
+                <td>0,35</td>
+            </tr>
+            <tr>
+                <td>6</td>
+                <td>kersen</td>
+                <td>0,40</td>
+            </tr>
+            <tr>
+                <td>7</td>
+                <td>aardbeien</td>
+                <td>0,45</td>
+            </tr>
+            <tr>
+                <td>8</td>
+                <td>mango</td>
+                <td>0,50</td>
+            </tr>
+            <tr>
+                <td>9</td>
+                <td>drijf</td>
+                <td>0,60</td>
             </tr>
         </table>
 
@@ -37,5 +91,12 @@
     <p>je bent <?php getAge(); ?> je korting is: </p>
 
     </form><!-- dit laten staan! -->
+
+    <footer>
+    <?php include 'footer.php';
+    ?>
+    </footer>
+
   </body>
+
 </html>
