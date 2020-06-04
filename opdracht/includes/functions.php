c123d29531b99fbf9ef99a8420fb8274aa1a6b92
Adjani Valerius <2068687@talnet.nl>
2020-06-04 11:51:36 +0100

    einopdracht , progress

--- opdracht/includes/functions.php
+++ opdracht/includes/functions.php
@@ -6,23 +6,39 @@ function getInput()
     echo $input;
 }
 
-
 function getProductNr()
 {
-    $productnummer = $_GET['productnummer'] ?? 'vul een productnummer in!';
-    echo $productnummer;
+    $productnummer = $_GET['productnummer'] ?? 0 ;
+    return $productnummer;
 }
 
-function getAmount()
-{
+function getAmount(){
     $aantalProd = $_GET['aantal'] ?? 'vul een aantal in!';
-    echo $aantalProd;
+    return $aantalProd;
+}
+
+function totaalPrijs()
+{
+    $itemsPrijs = array (0.1, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.60);
+
+                  //4        *        2  = 8
+    if(isset($_GET['aantal']) && isset($_GET['productnummer'])  ){
+        $totaalprijs = getAmount() * $itemsPrijs[ getProductNr() ];
+        echo $totaalprijs;
+
+    }
 }
 
 function getAge()
 {
     $leeftijd = $_GET['leeftijd'] ?? 'vul een leeftijd in!';
     echo $leeftijd;
+}
+
+function korting()
+{
+    if( $totaalPrijs < 16)
+        echo $korting
 }
 
 ?>
--- opdracht/producten.php
+++ opdracht/producten.php
@@ -4,13 +4,13 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Title</title>
+    <title>PRODUCTEN</title>
   </head>
   <body>
 
   <?php 
   include 'menu.php'; 
-  require 'includes/functions.php'; 
+  include 'includes/functions.php'; 
   ?>
 
 
@@ -19,8 +19,8 @@
 
     <p>Hieronder een lijst met producten die je bij ons kunt bestellen en de prijs per stuk:</p>
         
-<?php $itemsProducten = array ("appels", "peren", "ananas", "banaan", "sinasappel", "perzik", "kersen", "aardbeien", "mango", "druif");
-      $itemsPrijs = array ("0,10", "0.15", "0.20", "0.25", "0.30", "0.35", "0.40", "0.45", "0.50", "0.60");
+<?php $itemsProducten = array ("Appels", "peren", "ananas", "banaan", "sinasappel", "perzik", "kersen", "aardbeien", "mango", "druif");
+      $itemsPrijs = array (0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 1.0, 2.00, 4.00, 5.00);
 ?>
 
         <table>
@@ -31,63 +31,66 @@
             </tr>
             <tr>
                 <td>0</td>
-                <td>appels</td>
-                <td>0,10</td>
+                <td><?php echo $itemsProducten[0] ?></td>
+                <td><?php echo $itemsPrijs[0] ?></td>
             </tr>
             <tr>
                 <td>1</td>
-                <td>peren</td>
-                <td>0,15</td>
+                <td><?php echo $itemsProducten[1] ?></td>
+                <td><?php echo $itemsPrijs[1] ?></td>
             </tr>
             <tr>
                 <td>2</td>
-                <td>ananas</td>
-                <td>0,20</td>
+                <td><?php echo $itemsProducten[2] ?></td>
+                <td><?php echo $itemsPrijs[2] ?></td>
             </tr>
             <tr>
                 <td>3</td>
-                <td>banaan</td>
-                <td>0,25</td>
+                <td><?php echo $itemsProducten[3] ?></td>
+                <td><?php echo $itemsPrijs[3] ?></td>
             </tr>
             <tr>
                 <td>4</td>
-                <td>sinasappel</td>
-                <td>0,30</td>
+                <td><?php echo $itemsProducten[4] ?></td>
+                <td><?php echo $itemsPrijs[4] ?></td>
             </tr>
             <tr>
                 <td>5</td>
-                <td>perzik</td>
-                <td>0,35</td>
+                <td><?php echo $itemsProducten[5] ?></td>
+                <td><?php echo $itemsPrijs[5] ?></td>
             </tr>
             <tr>
                 <td>6</td>
-                <td>kersen</td>
-                <td>0,40</td>
+                <td><?php echo $itemsProducten[6] ?></td>
+                <td><?php echo $itemsPrijs[6] ?></td>
             </tr>
             <tr>
                 <td>7</td>
-                <td>aardbeien</td>
-                <td>0,45</td>
+                <td><?php echo $itemsProducten[7] ?></td>
+                <td><?php echo $itemsPrijs[7] ?></td>
             </tr>
             <tr>
                 <td>8</td>
-                <td>mango</td>
-                <td>0,50</td>
+                <td><?php echo $itemsProducten[8] ?></td>
+                <td><?php echo $itemsPrijs[8] ?></td>
             </tr>
             <tr>
                 <td>9</td>
-                <td>drijf</td>
-                <td>0,60</td>
+                <td><?php echo $itemsProducten[9] ?></td>
+                <td><?php echo $itemsPrijs[9] ?></td>
             </tr>
         </table>
 
     <p>Vul hieronder het productnummer in van het product wat je wilt bestellen en de hoeveelheid.</p> 
-    <p><label>productnummer: </label><input type="text" name="productnummer"> &nbsp;&nbsp;<label>aantal: </label><input type="text" name="aantal"></p><!-- dit laten staan! -->
+    <p><label>productnummer(#): </label><input type="text" name="productnummer"> &nbsp;&nbsp;<label>aantal: </label><input type="text" name="aantal"></p><!-- dit laten staan! -->
     
     <p>Vul je leeftijd in voor korting</p>
     <p><label>leeftijd: </label><input type="text" name="leeftijd"></p><!-- dit laten staan! -->
+
     <p><button type="submit" name="submit">Bereken totaal</button></p><!-- dit laten staan! -->
-    <p>voor <?php getAmount(); ?> x het product: <?php getProductNr(); ?> betaal je:</p>
+
+    <p>voor <?php echo getAmount('aantal'); ?> x het product: <?php echo $itemsProducten[ getProductNr('productnummer') ]; ?> betaal je: <?php echo getAmount() ?><?php totaalPrijs() ?></p>
+    
     <p>je bent <?php getAge(); ?> je korting is: </p>
 
     </form><!-- dit laten staan! -->
